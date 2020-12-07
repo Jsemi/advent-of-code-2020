907c36941618232b11664d2b4b65e0f8f2d7464a
Joona Semi <jsemi@users.noreply.github.com>
2020-12-07 17:56:00 +0200

    Solve day seven 🎅

---
+++ 07_handy_haversacks/haversacks_one.ts
@@ -0,0 +1,44 @@
+import { readInputToString } from "../helpers.ts";
+
+const data = readInputToString();
+
+export interface Bag {
+  count: number;
+  color: string;
+}
+
+function getInnerColors(rest: string): Bag[] | [] {
+  return rest === "no other bags" ? [] : rest.split(", ").map((others) => {
+    const [, count, color] = others.match(/(\d+) (\w+ \w+) bags?/) || [];
+    return { count: Number(count), color };
+  });
+}
+
+export function mapRules(data: string[]): Map<string, Bag[]> {
+  return data.reduce((rules, row) => {
+    const [, color, rest] = row.match(/(\w+ \w+) bags contain (.*)\./) || [];
+    rules.set(color, []);
+
+    const innerColors = getInnerColors(rest);
+    innerColors.forEach((inner) => rules.get(color).push(inner));
+
+    return rules;
+  }, new Map());
+}
+
+function traverse(rules: Map<string, Bag[]>, bag: string[]) {
+  const entry = rules.get(bag[0]) || [];
+  const colors = entry.reduce((acc, { color }) => {
+    acc.push(...traverse(rules, [color]));
+    return acc;
+  }, [...bag]);
+  return colors;
+}
+
+const rules = mapRules(data);
+const result =
+  [...rules.keys()].filter((key) =>
+    traverse(rules, [key]).includes("shiny gold")
+  ).length - 1;
+
+console.log(result);
---
+++ 07_handy_haversacks/haversacks_two.ts
@@ -0,0 +1,18 @@
+import { readInputToString } from "../helpers.ts";
+import { Bag, mapRules } from "./haversacks_one.ts";
+
+const data = readInputToString();
+
+function traverse(rules: Map<string, Bag[]>, bag: string): number {
+  const entry = rules.get(bag) || [];
+  const total = entry.reduce(
+    (acc, { count, color }) => acc += count + count * traverse(rules, color),
+    0,
+  );
+  return total;
+}
+
+const rules = mapRules(data);
+const result = traverse(rules, "shiny gold");
+
+console.log(result);
